381766e5ee7958fa14b67ec2d160f7b01867cfaf
PreranaPandit <preranapandit07@gmail.com>
2019-12-02 11:45:32 +0545

    Login Page Design In Process

---
+++ app/src/main/java/com/example/bloodbankcommunity/LoginAcitivity.java
@@ -0,0 +1,14 @@
+package com.example.bloodbankcommunity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_acitivity);
+    }
+}
